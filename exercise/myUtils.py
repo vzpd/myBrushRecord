6dc1bb09a907d3084e9b8b063f98b9088f58f102
vzpd <986358208@qq.com>
2020-04-17 05:28:10 +0800

    每日一题：合并区间

--- exercise/myUtils.py
+++ exercise/myUtils.py
@@ -4,9 +4,10 @@ import time
 def count_time(func):
     def int_time(*args, **kwargs):
         start_time = time.time()  # 程序开始时间
-        func(*args, **kwargs)
+        res = func(*args, **kwargs)
         over_time = time.time()  # 程序结束时间
         total_time = (over_time - start_time) * 1000
         print('程序%s运行用时%s毫秒' % (func.__name__, round(total_time, 3)))
+        return res
 
     return int_time
