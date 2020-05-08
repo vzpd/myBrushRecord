b33a9efa1e086a9bc210531bd8fbecfcfb73f796
vzpd <986358208@qq.com>
2020-05-08 15:23:33 +0800

    动态规划：比特位计数

--- exercise/动态规划_比特位计数.py
+++ exercise/动态规划_比特位计数.py
@@ -17,6 +17,7 @@ class Solution:
         #     else:
         #         dp.append(1 + dp[i - temp])
         # return dp
+        # 优化后
         dp = [0]
         temp = 1
         for i in range(1, num + 1):
@@ -26,6 +27,11 @@ class Solution:
             else:
                 dp.append(1 + dp[i - temp])
         return dp
+        # 最低有效位
+        # dp = [0]
+        # for i in range(1, num + 1):
+        #     dp.append(dp[i >> 1] + (i & 1))
+        # return dp
 
 
 s = Solution()
@@ -33,5 +39,5 @@ assert s.countBits(0) == [0]
 assert s.countBits(2) == [0, 1, 1]
 assert s.countBits(5) == [0, 1, 1, 2, 1, 2]
 s.countBits(10000000)
-#2160
-#1850
+# 2160
+# 1850
