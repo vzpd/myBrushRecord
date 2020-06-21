627ff2681aac1436d989516c72cfdb20fe6740c2
zhangpeng <986358208@qq.com>
2020-06-21 22:35:37 +0800

    周赛：0621更新

--- exercise/周赛_0621.py
+++ exercise/周赛_0621.py
@@ -355,7 +355,6 @@ class Solution:
 
 
 s = Solution()
-assert s.avoidFlood([2, 3, 0, 0, 3, 1, 0, 1, 0, 2, 2]) == []
 assert s.avoidFlood([0, 1, 1]) == []
 assert s.avoidFlood([1, 2, 3, 4]) == [-1, -1, -1, -1]
 assert s.avoidFlood([1, 2, 0, 0, 2, 1]) == [-1, -1, 2, 1, -1, -1]
