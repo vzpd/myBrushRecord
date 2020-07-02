ad593a1dc79213b76d1f80a5354535b6ae142765
vzpd <986358208@qq.com>
2020-07-02 10:38:56 +0800

    Merge branch 'test' of I:\pythonWorkSpace\myBrushRecords\myBrushRecord with conflicts.

--- exercise/每日一题_最长重复子数组.py
+++ exercise/每日一题_最长重复子数组.py
@@ -1,3 +1,8 @@
+import collections
+from typing import List
+
+from exercise.myUtils import timer
+
 """
 给两个整数数组 A 和 B ，返回两个数组中公共的、长度最长的子数组的长度。
 
@@ -7,74 +12,73 @@
 A: [1,2,3,2,1]
 B: [3,2,1,4,7]
 输出: 3
-解释:
+解释: 
 长度最长的公共子数组是 [3, 2, 1]。
 说明:
 
 1 <= len(A), len(B) <= 1000
 0 <= A[i], B[i] < 100
-"""
 
+注意:子序列->长度相同，元素相同，顺序可以不一致，下面的第一个求的是子序列
+     子数组->长度相同，元素相同，顺序相反,下面第二个求的是子数组
 """
-子数组：元素相同，个数相同，顺序相同
-子序列：元素相同，个数相同，顺序不一定相同
-
-此题二种解法：
-    1、动态规划
-    2、滑动窗口
-"""
-import collections
-from typing import List
-
-from exercise.myUtils import timer
 
 
 class Solution:
     @timer
     def findLength(self, A: List[int], B: List[int]) -> int:
-        # dict = collections.defaultdict(list)
-        # for i in range(len(A)):
-        #     dict[A[i]].append(i)
-        #
-        # maxl = 0
-        # downdp = [0] * (len(A) + 1)
-        # for i in range(len(B)):
-        #     tempdowndp = [0] * (len(A) + 1)
-        #     for x in dict[B[i]]:
-        #         tempdowndp[x + 1] = downdp[x] + 1
-        #         maxl = max(maxl, tempdowndp[x + 1])
-        #     downdp = tempdowndp
-        # return maxl
-        maxl = 0
-
-        def getl(a, b):
-            nonlocal maxl
-            length = min(len(a), len(b))
-            l = 0
-            for i in range(length):
-                if a[i] == b[i]:
-                    l += 1
-                else:
-                    maxl = max(maxl, l)
-                    l = 0
-            maxl = max(maxl, l)
-
+        dict = collections.defaultdict(list)
         for i in range(len(A)):
-            getl(A[i:], B)
-        for i in range(len(B)):
-            getl(A, B[i:])
+            dict[A[i]].append(i)
+
+        temp = [dict[x] for x in B]
+
+        gap = -1
+        for i in range(len(temp)):
+            if not temp[i]:
+                gap = i
+            for j in range(len(temp[i]) - 1, -1, -1):
+                if temp[i][j] <= gap:
+                    temp[i].pop(j)
+
+        gap = len(temp)
+        for i in range(len(temp) - 1, -1, -1):
+            if not temp[i]:
+                gap = i
+            for j in range(len(temp[i]) - 1, -1, -1):
+                if temp[i][j] >= gap:
+                    temp[i].pop(j)
+
+        maxl = 0
+        l = 0
+        for i in range(len(temp)):
+            if temp[i]:
+                l += 1
+                maxl = max(maxl, l)
+            else:
+                l = 0
 
         return maxl
 
 
+class Solution:
+    @timer
+    def findLength(self, A: List[int], B: List[int]) -> int:
+        maxl = 0
+        for i in range(len(A)):
+            for j in range(len(B)):
+                l = min(len(A) - i, len(B) - j)
+                for k in range(1, l + 1):
+                    if A[i:i + l] == B[j:j + l] or A[i:i + l] == B[j:j + l:-1]:
+                        maxl = max(maxl, l)
+        return maxl
+
+
 s = Solution()
-assert s.findLength([1, 2], [1, 2]) == 2
-assert s.findLength([69, 51, 94, 52, 72, 74, 65, 65, 99, 1], [65, 99, 82, 27, 43, 95, 9, 20, 13, 99]) == 2
-assert s.findLength([1, 2, 3, 4, 1], [3, 2, 1, 4, 7]) == 1
-assert s.findLength([1], [1]) == 1
-assert s.findLength([1], [2]) == 0
-assert s.findLength([1, 2], [2, 1]) == 1
-assert s.findLength([1, 2, 3], [1, 2, 3]) == 3
-assert s.findLength([1, 2, 3], [3, 2, 1]) == 1
-assert s.findLength([1, 2, 3], [1, 3, 2]) == 1
+assert s.findLength([0, 0, 0, 0, 1], [1, 0, 0, 0, 0]) == 4
 assert s.findLength([1, 2, 3, 2, 1], [3, 2, 1, 4, 7]) == 3
+assert s.findLength([1], [3]) == 0
+assert s.findLength([3], [3]) == 1
+assert s.findLength([3], [3, 2]) == 1
+assert s.findLength([3, 2], [3, 2]) == 2
+assert s.findLength([1, 2, 3, 4, 5, 6, 7], [1, 2, 3, 8, 5, 6, 7, 8]) == 3
